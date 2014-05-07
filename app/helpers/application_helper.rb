6e880789922e56bdafb8a8dbc20c7022720d2e7f
Pedro Pablo Aste Kompen <wachunei@gmail.com>
2014-05-07 09:45:31 -0400

    Primera version definitiva

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,11 @@
 module ApplicationHelper
 
-	def get_action_link(task) 
+	def get_action_link(task)
+		char = "✓"
 		if task.done? 
-			link_to "✔", task_mark_undone_path(task)
+			link_to char, task_mark_undone_path(task)
 		else 
-			link_to "✔", task_mark_done_path(task)
+			link_to char, task_mark_done_path(task)
 		end
 	end
 end
