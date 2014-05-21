cc649a78c9be8827ab3d0ad55a49cbab7edf779e
Hans Findel <hanstrax@gmail.com>
2014-05-21 11:02:26 -0400

    Fixtures and model tests

---
+++ test/README.md
@@ -0,0 +1,11 @@
+* Steps (for rails default test suite)
+
+1.- Watch the YML fixtures
+2.- Modify and try embeding some ruby
+
+3.- Modify the tests in test/models and try the assert true (rake test test/models/*)
+	Before this you should already have created a database for testing -> rake db:test:clone
+4.- Create some creation model tests
+5.- Test every (relevant) method in the model
+5.- Add some validation to the model - watch the tests fail
+
--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
 class TaskTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # test if this works
+  test "the truth" do
+    assert true
+  end
+
+
+  test "should (not) save task without title" do
+    task = Task.new
+    assert_not task.save
+  end
+
+  test "should  save task from fixture" do
+    task = tasks(:one)
+    assert task.valid?
+  end  
+
+  test "task one from fixture should be done" do 
+    task = tasks(:one)
+    assert task.done? 
+  end
+  
+  test "task two from fixture should not be done" do 
+    task = tasks(:two)
+    assert_not task.done? 
+  end
+  
+  test "task n=1 from fixture should not be done - mark it done" do 
+    task = tasks(:task_1)
+    assert_not task.done?, "task 1 is done from the beginning"
+    task.mark_done
+    assert task.done? 
+  end
+
+  test "task n=2 from fixture should be done - mark it done" do 
+    task = tasks(:task_2)
+    assert task.done?, "task 2 is not done from the beginning"
+    task.mark_undone
+    assert_not task.done? 
+  end
+
 end
