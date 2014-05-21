4ecc2e48cc3f0b3cf06f2e7a26421cedf4cbcafe
Hans Findel <hanstrax@gmail.com>
2014-05-21 11:49:28 -0400

    Add some tests to the task_controller

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -48,4 +48,33 @@ class TasksControllerTest < ActionController::TestCase
 
     assert_redirected_to tasks_path
   end
+
+
+  test "if task exists - its title should be on the page" do
+    task = tasks(:task_6)
+    get :index
+    assert_response :success
+    assert_select ".title" do |tasks_titles|
+      contained = false
+      tasks_titles.each do |task_title|
+        contained |= task_title.to_s.include?(task.title)
+      end
+      assert contained
+    end
+  end
+  
+  test "if task is deleted - its title should not be on the page" do
+    task = tasks(:task_3)
+    task.destroy
+
+    get :index
+    assert_response :success
+    assert_select ".title" do |tasks_titles|
+      contained = false
+      tasks_titles.each do |task_title|
+        contained |= task_title.to_s.include?(task.title)
+      end
+      assert_not contained
+    end
+  end
 end
